bb26c341bd1481a5273c4b83e45983a28ae522e1
Olha Hizhytska <olgainfotech@gmail.com>
2021-05-07 11:57:41 -0700

    add users validation

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,7 +1,30 @@
 const express = require('express');
 const router = express.Router();
+const { body, validationResult } = require('express-validator');
 
-// GET api/users public
-router.get('/', (req, res) => res.send('User route'));
+// POST api/users public
+router.post('/', [
+  body('name', 'Name is required') 
+    .not()
+    .isEmpty(),
+  body('email', 'Please include valide email')
+    .isEmail(),
+  body('password', 'Please enter a password with 6 or more characters')
+    .isLength({ min: 6 })
+],
+  (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() })
+  } 
+
+  res.send('User route')
+  }
+);
+
+
+
+
+
 
 module.exports = router;

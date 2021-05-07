bf58cdaf32170079938a5b5f551a31635d1479c7
Olha Hizhytska <olgainfotech@gmail.com>
2021-05-07 14:18:20 -0700

    add cheking if user exist and add avatar

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const { body, validationResult } = require('express-validator');
+const gravatar = require('gravatar');
+
+const User = require('../../models/User');
 
 // POST api/users public
 router.post('/', [
@@ -12,16 +15,41 @@ router.post('/', [
   body('password', 'Please enter a password with 6 or more characters')
     .isLength({ min: 6 })
 ],
-  (req, res) => {
+async (req, res) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() })
-  } 
+    return res.status(400).json({ errors: errors.array() })
+  } //send back error message
+  
+  const { name, email, password } = req.body;
 
-  res.send('User route')
+  try {
+  // See if user exists by email
+  let user = await User.findOne({ email });
+
+  if (user) {
+    return res.status(400).json({ errors: [{ msg: 'User already exists'}] });
   }
-);
 
+  const avatar = gravatar.url(email, {
+    s: '200',
+    r: 'pg',
+    d: 'mm'
+  })
+  
+  user = new User({
+    name,
+    email,
+    avatar,
+    password  
+  })
+  res.send('User route')
+  } catch(err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }    
+}
+);
 
 
 

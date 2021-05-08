a6aa4791e2ab2768a2c7f2ca821c3b385e2c5627
Olha Hizhytska <olgainfotech@gmail.com>
2021-05-08 16:55:07 -0700

    add route create and update profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../../middleware/auth');
+const { body, validationResult } = require('express-validator');
 
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
@@ -18,3 +19,55 @@ router.get('/me', auth, async (req, res) => {
     res.status(500).send('Server Error');
   }
 });
+
+// @route POST api/profile
+router.post('/', [ auth, [
+  body('address', 'Address is required')
+  .not()
+  .isEmpty(),
+  body('phoneNumber', 'Phone number is required')
+  .not()
+  .isEmpty()
+]
+], async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() })
+  }
+
+  const { address, bio, phoneNumber } = req.body;
+  
+  //Build profile object
+  const profileFields = {};
+  profileFields.user = req.user.id;
+  if(address) profileFields.address = address;
+  if(bio) profileFields.bio = bio;
+  if(phoneNumber) profileFields.phoneNumber = phoneNumber;
+
+  try {
+    let profile = await Profile.findOne({ user: req.user.id });
+    if(profile) {
+      //Update
+      profile = await Profile.findOneAndUpdate(
+        {user: req.user.id},
+        {$set: profileFields},
+        {new: true}
+      );
+
+      return res.json(profile);
+    }
+
+    // Create
+    profile = new Profile(profileFields);
+
+    await profile.save();
+    res.json(profile);
+
+  } catch(err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+
+module.exports = router;

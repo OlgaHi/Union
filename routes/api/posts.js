7ca3a22bee2b90eb71ac705d31fb478a99671d4b
Olha Hizhytska <olgainfotech@gmail.com>
2021-05-09 11:11:38 -0700

    add route create post

---
+++ routes/api/posts.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const router = express.Router();
+const { body, validationResult } = require('express-validator');
+const auth = require('../../middleware/auth');
+
+const User = require('../../models/User');
+const Post = require('../../models/Post');
+const { route } = require('./users');
+
+// POST api/posts
+router.post('/', [ auth, [
+  body('text', 'Text is required')
+  .not()
+  .isEmpty()
+]
+], 
+async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+  return res.status(400).json({ errors: errors.array() })
+}
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+
+    const newPost = new Post({
+    text: req.body.text,
+    name: user.name,
+    avatar: user.avatar,
+    user: req.user.id
+  })
+  
+  const post = await newPost.save();
+
+  res.json(post);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error')
+  }
+  
+});
+
+
+module.exports = router;

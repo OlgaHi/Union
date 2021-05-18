e1cae8c6d9b044c5c6ba01445fa79dcf050fbd95
Olha Hizhytska <olgainfotech@gmail.com>
2021-05-17 23:30:39 -0700

    add login action and reducer

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,6 +1,7 @@
 import React, { Fragment, useState } from 'react';
 import { Link } from 'react-router-dom';
-
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 const Login = () => {
   const [formData, setFormData] = useState({

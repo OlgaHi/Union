9a68babbfa7ff7004ed3944ca2a98f897898a596
Olha Hizhytska <olgainfotech@gmail.com>
2021-05-19 12:57:57 -0700

    add get current profile action

---
+++ client/src/reducers/profile.js
@@ -0,0 +1,14 @@
+const initialState = {
+  profile: null,
+  profiles: [],
+  loading: true,
+  error: {}
+}
+
+export default function(state = initialState, action) {
+  const { type, payload } = action;
+
+  switch(type) {
+    
+  }
+}

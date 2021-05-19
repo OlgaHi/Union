36977c10b7c152bc8bc43af512a71fbaf255c7be
Olha Hizhytska <olgainfotech@gmail.com>
2021-05-18 17:21:19 -0700

    add logout functionality

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -6,7 +6,8 @@ import {
   USER_LOADED, 
   AUTH_ERROR,
   LOGIN_SUCCESS,
-  LOGIN_FAIL
+  LOGIN_FAIL,
+  LOGOUT
 } from './types';
 import setAuthToken from '../utils/setAuthToken';
 
@@ -90,3 +91,8 @@ export const login = (email, password ) => async dispatch => {
     })
   }
 }
+
+// Logout
+export const logout = () => dispatch => {
+  dispatch({ type: LOGOUT });
+};
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { logout } from '../../actions/auth';
 
-const Navbar = () => {
+const Navbar = ({ auth: { isAuthenticated, loading}, logout }) => {
+  const authLinks = (
+    <ul>
+        <li>
+          <a onClick={logout} href="#!"><i className="fas fa-sign-out-alt"></i>{" "}Logout</a>
+        </li>
+    </ul>
+  )
+
+  const guestLinks = (
+    <ul>
+      <li>
+        <Link to="/profiles">Neighbors</Link>
+      </li>
+      <li>
+        <Link to="/register">Register</Link>
+      </li>
+      <li>
+        <Link to="/login">Login</Link>
+      </li>  
+    </ul>
+  )
+
   return (
     <nav className="navbar">
-      
       <h1><Link className="navbar-brand" to="/">
           <img
             src="https://cdn.pixabay.com/photo/2020/10/23/14/42/handshake-5678931__340.png"
@@ -12,20 +36,21 @@ const Navbar = () => {
             className="d-inline-block align-top"
             alt="logo"
             loading="lazy"
-          ></img><strong>Union</strong></Link></h1>
-      <ul>
-        <li>
-          <Link to="/profiles">Neighbors</Link>
-        </li>
-        <li>
-          <Link to="/register">Register</Link>
-        </li>
-        <li>
-          <Link to="/login">Login</Link>
-        </li>
-      </ul>
+          ></img><strong>Union</strong></Link>
+      </h1>
+      { !loading && (<Fragment>{ isAuthenticated ? authLinks : guestLinks}</Fragment>)}
+
     </nav>
   )
 }
 
-export default Navbar;
+Navbar.propTypes = {
+  logout: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+}
+
+const mapStateToProps = state => ({
+  auth: state.auth
+});
+
+export default connect(mapStateToProps, { logout })(Navbar);

b8cef604e971b9160655a1aa7287a62fbd557ae3
Olha Hizhytska <olgainfotech@gmail.com>
2021-05-19 23:25:27 -0700

    add action get all profiles

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -25,7 +25,23 @@ import {
   };
 
   // Get all profiles
-
+  export const getProfiles = () => async (dispatch) => {
+    dispatch({ type: CLEAR_PROFILE });
+  
+    try {
+      const res = await axios.get('/api/profile');
+  
+      dispatch({
+        type: GET_PROFILES,
+        payload: res.data
+      });
+    } catch (err) {
+      dispatch({
+        type: PROFILE_ERROR,
+        payload: { msg: err.response.statusText, status: err.response.status }
+      });
+    }
+  };
 
 
 
